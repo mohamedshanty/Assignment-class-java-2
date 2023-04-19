d3ba469686ebe309da4d6d89aa3cfcc39dd6c5fa
Mohamed salem <117261174+mohamedshanty@users.noreply.github.com>
2023-04-19 22:48:41 +0300

    Question 2

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+D
+src/Assignment2.java,5\c\5c1cceb043e7a24391e997653222d0e099c6347e
--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Assignment2 {
     public static void main(String[] args){
         /*Write a Java program to find the sum of first 10 natural
         numbers like: 1,2,3,4,5,6,7,8,9,10*/
-        int sum = 0;
+        /*int sum = 0;
         for (int i = 0; i <10 ; i++){
             sum += i;
             sum++;
         }
-        System.out.println("The sum of first 10 natural numbers is: "+sum);
+        System.out.println("The sum of first 10 natural numbers is: "+sum);*/
+
+
+
+        /*Write a program in Java to display n terms of natural
+        number and their sum */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = input.nextInt();
+        System.out.print("The first " + number + " natural numbers are: ");
+        int sum = 0;
+        for (int i = 1; i <=number; i++){
+            System.out.print(i+"");
+            sum += i;
+        }
+        System.out.println("\nThe sum of the first " + number +" natural numbers is: " + sum);
     }
 }
